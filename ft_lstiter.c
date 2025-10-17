515ccc0c05c70044ab59628ac99487d0f40b4515
Bilal Errabia <berrabia@c1r4p14.1337.ma>
2025-10-17 18:23:37 +0100

    share

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -5,26 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: berrabia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/16 16:57:31 by berrabia          #+#    #+#             */
-/*   Updated: 2025/10/16 16:57:54 by berrabia         ###   ########.fr       */
+/*   Created: 2025/10/17 18:21:35 by berrabia          #+#    #+#             */
+/*   Updated: 2025/10/17 18:21:41 by berrabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list	*ptr;
+    t_list *ptr;
 
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
-	ptr = *lst;
-	while (ptr->next)
-		ptr = ptr->next;
-	ptr->next = new;
+    if (!f)
+        return;
+    ptr = lst;
+    while (ptr)
+    {
+        f(ptr->content);
+        ptr = ptr->next;
+    }
 }
